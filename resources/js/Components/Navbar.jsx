1624c2217b001d650c92bca79dd14f0a38786c57
AhenkReptile <kaifan606.sh@gmail.com>
2023-05-24 12:53:38 +0700

    add code

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,33 +1,49 @@
-const Navbar = () => {
+import { Link } from "@inertiajs/react";
+
+const Navbar = ({user}) => {
     return (
-        <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">NaoKids</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered" />
-    </div>
-    <div className="dropdown dropdown-end">
-      <label tabindex="0" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <div className="navbar bg-base-100 fixed z-50 bg-transparent" id="navbar">
+            <div className="flex-none">
+                <div className="dropdown">
+                <button className="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </button>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-white rounded-box w-52 text-black hover:text-stone-800">
+                    <li><Link href={"/"}>Home</Link></li>
+                    <li><Link href={"/Products"}>Category</Link></li>
+                    <li><Link href={'/Homepage'}>About</Link></li>
+                </ul>
+                </div>
+            </div>
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="flex-none">
+            <div className="dropdown dropdown-end">
+            <button className="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                </button>
+            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+{!user ?
+    <>
+    <li><Link href={"/profile.edit"}>Profile</Link></li>
+                <li><Link href={"/"} method="post">Logout</Link></li>
+</>
+:
+<><li><Link href={"/login"}>Login</Link></li>
+<li><Link href={"/register"}>Register</Link></li>
+
+                </>
+                     }
+
+            </ul>
+            </div>
+            </div>
+
         </div>
-      </label>
-      <ul tabindex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-    )
-}
+
+    );
+};
+
 
 export default Navbar
